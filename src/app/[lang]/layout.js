fe341d90f770686ffd70195589319336ecd86ae3
UNRIVALLEDKING <adityakumarunrivalledking@gmail.com>
2023-09-23 20:35:29 +0530

    manual build

--- src/app/[lang]/layout.js
+++
@@ -1,8 +0,0 @@
-export const metadata = {
-  title: 'Blog - EJY Health',
-  description: 'Official Blog Portal of EJY Health.',
-};
-
-export default function RootLayout({ children }) {
-  return <>{children}</>;
-}
--- src/app/[lang]/page.js
+++
@@ -1,4 +0,0 @@
-export default function page(params) {
-  console.log('params', params);
-  return <>Yahallo! in {params.params.lang}</>;
-}
